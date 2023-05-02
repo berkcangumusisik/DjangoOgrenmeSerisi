d9371ac9c15250728a422395d813e4b2662cea72
Berkcan Gümüşışık <gumusisikberkcan@gmail.com>
2023-05-02 16:35:59 +0300

    Movie app part 1 tamamlandı.

--- movieapp/movieapp/movies/urls.py
+++ movieapp/movieapp/movies/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, ),
-    path('movies', views.movies),
-    path('movies/<slug:slug>', views.movies_details),
+    path('', views.index, name ="home_page"),
+    path('movies', views.movies, name="movies_page"),
+    path('movies/<slug:slug>', views.movies_details, name="movies_details"),
     
 ]
 """ 
--- movieapp/movieapp/movies/views.py
+++ movieapp/movieapp/movies/views.py
@@ -1,14 +1,80 @@
+from datetime import date
 from django.shortcuts import render
 from django.http import HttpResponse
 
+data = {
+    "movies": [
+        {
+            "title": "film adı 1",
+            "description": "film açıklama 1",
+            "imageUrl": "m1.jpg",
+            "coverImage" : "cover1.jpg",
+            "slug": "film-adi-1",
+            "language": "english",
+            "date": date(2021,10,10)
+        },
+        {
+            "title": "film adı 2",
+            "description": "film açıklama 2",
+            "imageUrl": "m2.jpg",
+            "coverImage" : "cover1.jpg",
+            "slug": "film-adi-2",
+            "language": "english",
+            "date": date(2021,5,10)
+        },
+        {
+            "title": "film adı 3",
+            "description": "film açıklama 3",
+            "imageUrl": "m3.jpg",
+            "coverImage" : "cover2.jpg",
+            "slug": "film-adi-3",
+            "language": "english",
+            "date": date(2021,10,5)
+        },
+        {
+            "title": "film adı 4",
+            "description": "film açıklama 4",
+            "imageUrl": "m4.jpg",
+            "coverImage" : "cover3.jpg",
+            "slug": "film-adi-4",
+            "language": "english",
+            "date": date(2020,10,5)
+        }
+    ],
+    "sliders": [
+        {
+            "sliderImage": "slider1.jpg",
+            'url': 'film-adi-1',
+        },{
+            "sliderImage": "slider2.jpg",
+            "url": "film-adi-2"
+        },{
+            "sliderImage": "slider3.jpg",
+            "url": "film-adi-3"
+        }
+    ]
+}
+
+
+# Create your views here.
 
 def index(request):
-    return render(request, "index.html")
-
+    movies = data["movies"][-4:]
+    sliders = data["sliders"]
+    return render(request, 'index.html', {
+        "movies": movies,
+        "sliders" : sliders
+    })
 
 def movies(request):
-    return render(request, "movies.html")
-
+    movies = data["movies"]
+    return render(request, 'movies.html', {
+        "movies": movies
+    })
 
 def movies_details(request, slug):
-    return render(request, "movies_details.html", {"slug": slug})
+    movies = data["movies"]
+    seleceted_movie = {movie for movie in movies if movie["slug"] == slug}
+    return render(request, 'movies_details.html', {
+        "movie": selected_movie
+})
