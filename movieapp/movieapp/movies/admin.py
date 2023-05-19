c43ca6bee6ae7c12d1920e513734bb5474afab1d
Berkcan Gümüşışık <gumusisikberkcan@gmail.com>
2023-05-19 20:02:37 +0300

    Filmler uygulamasında ilerlendi

--- movieapp/movieapp/movies/admin.py
+++ movieapp/movieapp/movies/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from movies.models import Movie, Person, Genre, Contact, Video
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    prepopulated_fields = {'slug': ('title',) }
+    list_filter = ('genres','language',)
+    search_fields = ('title','description',)
 
-# Register your models here.
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('full_name','gender','duty_type')
+    list_filter = ('gender','duty_type')
+    search_fields = ('title','last_name')
+
+admin.site.register(Movie)
+admin.site.register(Person)
+admin.site.register(Contact)
+admin.site.register(Genre)
+admin.site.register(Video)
